740b2182454485bacb0c8cb501c81c203272442e
Matt <matthewjpetroff@gmail.com>
2020-07-23 22:05:36 -0400

    Finish functional app

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function randomChoice() {
 	choice = Math.floor(Math.random() * Math.floor(3));
+	
 	if (choice === 0) {
 		return 'rock';
 	} else if (choice === 1) {
@@ -27,8 +28,20 @@ function result(playerChoice, computerChoice) {
 	}
 }	
 
+function reset() {
+	document.getElementById('rock').src = 'images/rock.png';
+	document.getElementById('paper').src = 'images/paper.png';
+	document.getElementById('scissors').src = 'images/scissors.png';
+	document.getElementById('computer-rock').src = 'images/rock.png';
+	document.getElementById('computer-paper').src = 'images/paper.png';
+	document.getElementById('computer-scissors').src = 'images/scissors.png';
+}
+
 function play(choice) {
+	reset();
 	const computerChoice = randomChoice();
 	const outcome = result(choice, computerChoice);
+	document.getElementById(choice).src = `images/selected-${choice}.png`;
+	document.getElementById(`computer-${computerChoice}`).src = `images/selected-${computerChoice}.png`;
 	document.getElementById('result').innerHTML = `Result: ${outcome}`;
 }
